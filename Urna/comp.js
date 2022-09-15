6b45a07961c88e6962b819610338296faa4eb689
LaraeLua <111380182+LaraeLua@users.noreply.github.com>
2022-09-15 11:17:01 -0300

    Update comp.js

---
+++ Urna/comp.js
@@ -0,0 +1,18 @@
+    var num1 = document.getElementById('1');
+    var num2 = document.getElementById('2');
+    var num3 = document.getElementById('3');
+    var num4 = document.getElementById('4');
+    var num5 = document.getElementById('5');
+    var num6 = document.getElementById('6');
+    var num7 = document.getElementById('7');
+    var num8 = document.getElementById('8');
+    var num9 = document.getElementById('9');
+
+   switch (num1,num2,num3,num4,num5,num6,num7,num8,num9) {
+    case num1:
+         
+        break;
+   
+    default:
+        break;
+   }
