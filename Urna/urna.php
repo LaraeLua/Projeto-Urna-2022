1355ef4564535a85fba6c7379234f32f3969dcfd
LaraeLua <111380182+LaraeLua@users.noreply.github.com>
2022-09-15 10:40:09 -0300

    Update urna.php

--- Urna/urna.php
+++ Urna/urna.php
@@ -1,24 +1,38 @@
-<?php
-    require_once "comp.js";
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vamos votar?</title>
+    <script src="botao.js"></script>
 </head>
 <body>
-    <form method="post">
+        <h2>Deputado Federal</h2>
+        
         <p>Número:</p>
             <input size="1" maxlength="1" type="text" />
             <input size="1" maxlength="1" type="text" />
+            <input size="1" maxlength="1" type="text" />
+            <input size="1" maxlength="1" type="text" />
         <p>Nome:</p>
+            <input size="5" maxlength="5" type="text" />
 
+            <br><br><button onclick="botao()">Branco </button><br><br>
 
-    </form>  
-        
+            
+            <div class="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar com grupos de botões">
+                <div class="btn-group" role="group" aria-label="Primeiro grupo">
+                  <button type="button" class="btn btn-secondary">1</button>
+                  <button type="button" class="btn btn-secondary">2</button>
+                  <button type="button" class="btn btn-secondary">3</button><br>
+                  <button type="button" class="btn btn-secondary">4</button>
+                  <button type="button" class="btn btn-secondary">5</button>
+                  <button type="button" class="btn btn-secondary">6</button><br>
+                  <button type="button" class="btn btn-secondary">7</button>
+                  <button type="button" class="btn btn-secondary">8</button>
+                  <button type="button" class="btn btn-secondary">9</button><br>
+                  <button type="button" class="btn btn-secondary">0</button>
+                
+                </div>
 </body>
 </html>
 
