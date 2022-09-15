3cdcaa1bfba0c9d3dfdcfe903964ff6a19051452
LaraeLua <111380182+LaraeLua@users.noreply.github.com>
2022-09-15 10:39:29 -0300

    Add files via upload

---
+++ Urna/urna.php
@@ -0,0 +1,24 @@
+<?php
+    require_once "comp.js";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vamos votar?</title>
+</head>
+<body>
+    <form method="post">
+        <p>Número:</p>
+            <input size="1" maxlength="1" type="text" />
+            <input size="1" maxlength="1" type="text" />
+        <p>Nome:</p>
+
+
+    </form>  
+        
+</body>
+</html>
+
